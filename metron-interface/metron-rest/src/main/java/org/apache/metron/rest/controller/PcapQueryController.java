3adb57b5825d2c40bda93ac46e6edc3ddf9a2b5a
msumbul <msumbul@10.200.10.172>
2018-04-05 15:10:06 +0100

    First draft implementation of new rest api to not keep an open connection between the server and the client

--- metron-interface/metron-rest/src/main/java/org/apache/metron/rest/controller/PcapQueryController.java
+++ metron-interface/metron-rest/src/main/java/org/apache/metron/rest/controller/PcapQueryController.java
@@ -16,34 +16,29 @@
 package org.apache.metron.rest.controller;
 
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.io.IOException;
-import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
 import org.apache.metron.rest.RestException;
-import org.apache.hadoop.fs.Path;
 import org.apache.metron.common.Constants;
 import org.apache.metron.pcap.PcapHelper;
 import org.apache.metron.rest.model.PcapRequest;
 import org.apache.metron.rest.service.impl.PcapQueryServiceAsyncImpl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.apache.metron.rest.service.impl.PcapQueryServiceImpl;
-import org.apache.metron.rest.util.PcapQueryResult;
+import org.apache.metron.rest.util.PcapsResponse;
+import org.apache.metron.rest.util.pcapQueryThread;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -54,6 +49,83 @@ import org.springframework.web.context.request.async.DeferredResult;
 @RestController
 @RequestMapping("/api/v1/pcap")
 public class PcapQueryController {
+
+    List<pcapQueryThread> lPcapQueryThread = new ArrayList<>();
+
+    //Rest api to sumbit complete async pcap query that will not keep an open connection to the client
+    @ApiOperation(value = "Submit a pcap job to found specific paquets")
+    @ApiResponses({
+        @ApiResponse(message = "Return the id of the query running on the backend", code = 200)
+    })
+    @RequestMapping(value = "/pcapqueryfilterasync/submit", method = RequestMethod.POST)
+    public ResponseEntity<String> submitAsyncPcapQuery(@RequestBody PcapRequest pcapRequest
+    ) throws RestException, IOException {
+        pcapQueryThread t = new pcapQueryThread(pcapRequest);
+        lPcapQueryThread.add(t);
+        t.start();
+        return new ResponseEntity<>(t.getIdQuery(), HttpStatus.CREATED);
+    }
+
+    //Get status of a pcap query
+    @ApiOperation(value = "Get status of a pcap query")
+    @ApiResponses({
+        @ApiResponse(message = "Return the status of the query running on the backend", code = 200)
+    })
+    @RequestMapping(value = "/pcapqueryfilterasync/status", method = RequestMethod.POST)
+    public ResponseEntity<String> getAsyncPcapQueryStatus(@RequestParam(value = "idQuery") String idQuery
+    ) throws RestException, IOException {
+
+        pcapQueryThread t = pcapQueryThread.findQueryInList(lPcapQueryThread, idQuery);
+        if (t == null) {
+            return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(t.getStatus(), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Get the result of a pcap query")
+    @ApiResponses({
+        @ApiResponse(message = "Return the result of the query running on the backend", code = 200)
+    })
+    @RequestMapping(value = "/pcapqueryfilterasync/result", method = RequestMethod.POST)
+    public ResponseEntity<PcapsResponse> getAsyncPcapQueryResult(@RequestParam(value = "idQuery") String idQuery
+    ) throws RestException, IOException {
+
+        pcapQueryThread t = pcapQueryThread.findQueryInList(lPcapQueryThread, idQuery);
+
+        if(t == null){
+            return new ResponseEntity<>(new PcapsResponse(), HttpStatus.NOT_FOUND);
+        }
+        if(t.getPcapsReponse() == null || t.getPcapsReponse().getResponseSize() == 0){
+            return new ResponseEntity<>(new PcapsResponse(), HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(t.getPcapsReponse(), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Clear query")
+    @ApiResponses({
+        @ApiResponse(message = "Clear the result of the query on backendside", code = 200)
+    })
+    @RequestMapping(value = "/pcapqueryfilterasync/clear", method = RequestMethod.POST)
+    public ResponseEntity<String> clearAsyncPcapQueryResult(@RequestParam(value = "idQuery") String idQuery
+    ) throws RestException, IOException {
+
+        pcapQueryThread t = pcapQueryThread.findQueryInList(lPcapQueryThread, idQuery);
+        lPcapQueryThread.remove(t);
+        t.setPcapsReponse(new PcapsResponse());
+
+        return new ResponseEntity<>("Done", HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "List queries")
+    @ApiResponses({
+        @ApiResponse(message = "list the queries in memory on the rest api", code = 200)
+    })
+    @RequestMapping(value = "/pcapqueryfilterasync/listquery", method = RequestMethod.POST)
+    public ResponseEntity<List<String>> getListQueries() throws RestException, IOException {
+
+        return new ResponseEntity<>(pcapQueryThread.getListQueries(lPcapQueryThread), HttpStatus.OK);
+    }
 
     /*
     @RequestMapping(value = "/pcapqueryfilter", method = RequestMethod.GET)
@@ -86,8 +158,7 @@ public class PcapQueryController {
        return null;
     }
 
-    */
-    
+     */
     /////////////////////////
     /////////////////////////
     /*
@@ -141,7 +212,7 @@ public class PcapQueryController {
 
         return deferred;
     }
-*/
+     */
     @ApiOperation(value = "Submit a pcap job to found specific paquet")
     @ApiResponses({
         @ApiResponse(message = "Return pcap object containing packets", code = 200)
@@ -161,7 +232,7 @@ public class PcapQueryController {
                             put(Constants.Fields.SRC_ADDR.getName(), pcapRequest.getSrcIp());
                         }
                         if (pcapRequest.getDstIp() != null) {
-                            put(Constants.Fields.DST_ADDR.getName(), pcapRequest.getDstIp() );
+                            put(Constants.Fields.DST_ADDR.getName(), pcapRequest.getDstIp());
                         }
                         if (pcapRequest.getSrcPort() != null) {
                             put(Constants.Fields.SRC_PORT.getName(), pcapRequest.getSrcPort());
@@ -188,7 +259,6 @@ public class PcapQueryController {
         return deferred;
     }
 
-    
     private static boolean isValidPort(String port) {
         if (port != null && !port.equals("")) {
             try {
