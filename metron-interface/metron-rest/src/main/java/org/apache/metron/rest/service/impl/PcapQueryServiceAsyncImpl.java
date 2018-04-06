c357fe45f8225b00c9b5daa2daad8b6910c35eb3
msumbul <msumbul@10.200.10.172>
2018-04-06 18:19:10 +0100

    develop function to start pcapquery script

--- metron-interface/metron-rest/src/main/java/org/apache/metron/rest/service/impl/PcapQueryServiceAsyncImpl.java
+++ metron-interface/metron-rest/src/main/java/org/apache/metron/rest/service/impl/PcapQueryServiceAsyncImpl.java
@@ -17,9 +17,16 @@ package org.apache.metron.rest.service.impl;
 
 import com.google.common.collect.Lists;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.metron.common.hadoop.SequenceFileIterable;
 import org.apache.metron.common.utils.timestamp.TimestampConverters;
 import org.apache.metron.pcap.filter.fixed.FixedPcapFilter;
@@ -30,12 +37,16 @@ import org.apache.metron.rest.config.PcapConfig;
 import org.apache.metron.rest.util.PcapsResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.apache.metron.rest.model.PcapRequest;
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author msumbul We also reuse code from metron-api project
  */
-
 public class PcapQueryServiceAsyncImpl {
 
     private static ThreadLocal<Configuration> CONFIGURATION = new ThreadLocal<Configuration>() {
@@ -43,7 +54,7 @@ public class PcapQueryServiceAsyncImpl {
         @Override
         protected Configuration initialValue() {
             Configuration config = new PcapConfig().getConfiguration();
-            
+
             return config;
         }
     };
@@ -53,8 +64,6 @@ public class PcapQueryServiceAsyncImpl {
     protected PcapJob getQueryUtil() {
         return queryUtil;
     }
-
-    
 
     public ResponseEntity getPcapsByIdentifiers(Map<String, String> query, long startTime, long endTime, int numReducers) throws IOException, RestException {
         PcapsResponse response = new PcapsResponse();
@@ -73,20 +82,20 @@ public class PcapQueryServiceAsyncImpl {
             //convert to nanoseconds since the epoch
             startTime = TimestampConverters.MILLISECONDS.toNanoseconds(startTime);
             endTime = TimestampConverters.MILLISECONDS.toNanoseconds(endTime);
-            System.out.println("Query received: {}"+ query);
+            System.out.println("Query received: {}" + query);
             System.out.println("Configuration");
             System.out.println(CONFIGURATION.get());
             PcapConfig pcapConfig = new PcapConfig();
             System.out.println("Configuration mapreduce.framework.name " + pcapConfig.getConfiguration().get("mapreduce.framework.name"));
             results = getQueryUtil().query(new org.apache.hadoop.fs.Path(pcapConfig.getPcapSourcePath()),
-                     new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
-                     startTime,
-                     endTime,
-                     numReducers,
-                     query,
-                     pcapConfig.getConfiguration(),
-                     FileSystem.get(pcapConfig.getConfiguration()),
-                     new FixedPcapFilter.Configurator()
+                    new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
+                    startTime,
+                    endTime,
+                    numReducers,
+                    query,
+                    pcapConfig.getConfiguration(),
+                    FileSystem.get(pcapConfig.getConfiguration()),
+                    new FixedPcapFilter.Configurator()
             );
             response.setPcaps(results != null ? Lists.newArrayList(results) : null);
         } catch (Exception e) {
@@ -99,8 +108,8 @@ public class PcapQueryServiceAsyncImpl {
         }
         return new ResponseEntity<>(response.getPcaps(), HttpStatus.OK);
     }
-    
-     public PcapsResponse getPcapsByIdentifiersAsync(Map<String, String> query, long startTime, long endTime, int numReducers) throws IOException, RestException {
+
+    public PcapsResponse getPcapsByIdentifiersAsync(Map<String, String> query, long startTime, long endTime, int numReducers, PcapConfig pcapConfig) throws IOException, RestException {
         PcapsResponse response = new PcapsResponse();
         SequenceFileIterable results = null;
         try {
@@ -110,20 +119,21 @@ public class PcapQueryServiceAsyncImpl {
             if (endTime < 0) {
                 endTime = System.currentTimeMillis();
             }
-            
+
             //convert to nanoseconds since the epoch
             startTime = TimestampConverters.MILLISECONDS.toNanoseconds(startTime);
             endTime = TimestampConverters.MILLISECONDS.toNanoseconds(endTime);
-            System.out.println("Query received: {}"+ query);
+            System.out.println("Query received: {}" + query);
             System.out.println("Configuration");
             System.out.println(CONFIGURATION.get());
-            PcapConfig pcapConfig = new PcapConfig();
+            //   PcapConfig pcapConfig = new PcapConfig();
+
             System.out.println("Configuration mapreduce.framework.name " + pcapConfig.getConfiguration().get("mapreduce.framework.name"));
             System.out.println("Debug: We are going to call getQueryUtil().query in class pcapqueryserviceasyncimpl ");
             System.out.println("Debug: source path: " + pcapConfig.getPcapSourcePath());
             System.out.println("Debug: destination path: " + pcapConfig.getPcapOutputPath());
             PcapJob j = new PcapJob();
-            results = j.query(new org.apache.hadoop.fs.Path(pcapConfig.getPcapSourcePath()),
+            /*  results = j.query(new org.apache.hadoop.fs.Path(pcapConfig.getPcapSourcePath()),
                      new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
                      startTime,
                      endTime,
@@ -133,18 +143,34 @@ public class PcapQueryServiceAsyncImpl {
                      FileSystem.get(pcapConfig.getConfiguration()),
                      new FixedPcapFilter.Configurator()
             );
-             /*
+             */
+            System.out.println("Just before create job");
+            Job jj = j.createJob(new org.apache.hadoop.fs.Path(pcapConfig.getPcapSourcePath()),
+                    new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
+                    startTime,
+                    endTime,
+                    numReducers,
+                    query,
+                    pcapConfig.getConfiguration(),
+                    FileSystem.get(pcapConfig.getConfiguration()),
+                    new FixedPcapFilter.Configurator()
+            );
+            System.out.println("Just before submit");
+            jj.submit();
+
+            System.out.println("Just after submit");
+
             results = getQueryUtil().query(new org.apache.hadoop.fs.Path(pcapConfig.getPcapSourcePath()),
-                     new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
-                     startTime,
-                     endTime,
-                     numReducers,
-                     query,
-                     pcapConfig.getConfiguration(),
-                     FileSystem.get(pcapConfig.getConfiguration()),
-                     new FixedPcapFilter.Configurator()
+                    new org.apache.hadoop.fs.Path(pcapConfig.getPcapOutputPath()),
+                    startTime,
+                    endTime,
+                    numReducers,
+                    query,
+                    pcapConfig.getConfiguration(),
+                    FileSystem.get(pcapConfig.getConfiguration()),
+                    new FixedPcapFilter.Configurator()
             );
-            */
+
             System.out.println("Debug: We finished the method getQueryUtil().query in class pcapqueryserviceasyncimpl ");
             System.out.println("Debug query function next line");
             response.setPcaps(results != null ? Lists.newArrayList(results) : null);
@@ -158,5 +184,110 @@ public class PcapQueryServiceAsyncImpl {
             }
         }
         return response;
+    }
+
+    public PcapsResponse getPcapsLinuxProcess(PcapRequest pcapRequest, String idQuery) {
+        SequenceFileIterable results = null;
+        Runtime rt = Runtime.getRuntime();
+        PcapsResponse response = new PcapsResponse();
+
+        String cmdToExec = "/usr/hcp/current/metron/bin/pcap_query.sh fixed";
+
+        if (!pcapRequest.getSrcIp().isEmpty()) {
+            cmdToExec = cmdToExec + " --ip_src_addr " + pcapRequest.getSrcIp();
+        }
+        if (!pcapRequest.getSrcPort().isEmpty()) {
+            cmdToExec = cmdToExec + " --ip_src_port " + pcapRequest.getSrcPort();
+        }
+        if (!pcapRequest.getDstIp().isEmpty()) {
+            cmdToExec = cmdToExec + " --ip_dst_addr " + pcapRequest.getDstIp();
+        }
+        if (!pcapRequest.getDstPort().isEmpty() && Long.valueOf(pcapRequest.getDstPort()) > 0) {
+            cmdToExec = cmdToExec + " --ip_dst_port " + pcapRequest.getDstPort();
+        }
+
+        if (!pcapRequest.getProtocol().isEmpty()) {
+            cmdToExec = cmdToExec + " --protocol " + pcapRequest.getProtocol();
+        }
+
+        cmdToExec = cmdToExec + " -st " + pcapRequest.getStartTime() + " -bop /tmp/" + idQuery + " >>/tmp/log/pcapjob 2>&1";
+
+        // rt.exec(cmdToExec);
+        String workingDir = "/tmp/" + idQuery;
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+
+            Process process;
+            List<String> lCmd = getCommandList("mkdir " + workingDir);
+            System.out.println(lCmd);
+            pb.command(lCmd);
+            process = pb.start();
+            process.waitFor();
+            pb.directory(new File(workingDir));
+
+            lCmd = getCommandList(cmdToExec);
+            System.out.println(lCmd);
+            pb.command(lCmd);
+
+            process = pb.start();
+            process.waitFor();
+            Configuration conf = new Configuration();
+            results = readResults(new Path(workingDir), conf, FileSystem.get(conf));
+        } catch (IOException ex) {
+            Logger.getLogger(PcapQueryServiceAsyncImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(PcapQueryServiceAsyncImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.setPcaps(results != null ? Lists.newArrayList(results) : null);
+
+        return response;
+    }
+
+    private SequenceFileIterable readResults(String outputPath, Configuration config) throws IOException {
+        List<Path> files = new ArrayList<>();
+        files = getAllFiles(new File(outputPath));
+
+        Collections.sort(files, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return new SequenceFileIterable(files, config);
+    }
+
+    private SequenceFileIterable readResults(Path outputPath, Configuration config, FileSystem fs) throws IOException {
+        List<Path> files = new ArrayList<>();
+        for (RemoteIterator<LocatedFileStatus> it = fs.listFiles(outputPath, false); it.hasNext();) {
+            Path p = it.next().getPath();
+            if (p.getName().equals("_SUCCESS")) {
+                fs.delete(p, false);
+                continue;
+            }
+            files.add(p);
+        }
+
+        Collections.sort(files, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return new SequenceFileIterable(files, config);
+    }
+
+    private static List<Path> getAllFiles(File curDir) {
+        System.out.println("We are in list files");
+        List<Path> listFiles = new ArrayList<>();
+        File[] filesList = curDir.listFiles();
+        for (File f : filesList) {
+
+            if (f.isFile()) {
+                System.out.println(f.getAbsolutePath());
+                listFiles.add(new Path(f.getAbsolutePath()));
+            }
+        }
+        return listFiles;
+    }
+
+    private List<String> getCommandList(String cmd) {
+        List<String> lCmd = new ArrayList<>();
+        String[] cmdSplit = cmd.split(" ");
+
+        for (int i = 0; i < cmdSplit.length; i++) {
+            lCmd.add(cmdSplit[i]);
+
+        }
+        return lCmd;
     }
 }
