8a5f83fa55339944162d413d80c82e9a19403712
msumbul <msumbul@10.200.10.172>
2018-04-17 16:49:41 +0100

    new response class for the pcap rest api

---
+++ metron-interface/metron-rest-client/src/main/java/org/apache/metron/rest/model/PcapResponse.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.metron.rest.model;
+
+/**
+ *
+ * @author msumbul
+ */
+public class PcapResponse {
+    
+    private String status;
+    private String idQuery;
+    private String percentage;
+    private String json;
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the idQuery
+     */
+    public String getIdQuery() {
+        return idQuery;
+    }
+
+    /**
+     * @param idQuery the idQuery to set
+     */
+    public void setIdQuery(String idQuery) {
+        this.idQuery = idQuery;
+    }
+
+    /**
+     * @return the percentage
+     */
+    public String getPercentage() {
+        return percentage;
+    }
+
+    /**
+     * @param percentage the percentage to set
+     */
+    public void setPercentage(String percentage) {
+        this.percentage = percentage;
+    }
+
+    /**
+     * @return the json
+     */
+    public String getJson() {
+        return json;
+    }
+
+    /**
+     * @param json the json to set
+     */
+    public void setJson(String json) {
+        this.json = json;
+    }
+}
